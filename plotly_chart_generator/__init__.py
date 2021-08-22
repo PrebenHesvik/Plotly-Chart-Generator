2853b93f1a102df266a35df2b12c60a33b75c557
Preben Hesvik <Prebenhesvik@hotmail.com>
2021-08-22 12:11:10 +0200

    reverted import structure changes

--- plotly_chart_generator/__init__.py
+++ plotly_chart_generator/__init__.py
@@ -6,6 +6,5 @@ from . dot_chart import dot_chart
 from . histogram import histogram
 from . sunburst_chart import sunburst_chart
 from . box_plot import box_plot
-from . chart_styles import bar_headings, bar_values, chart_annotation, chart_colors, chart_styles, shape, theme
-
+from . chart_styles import chart_styles
 from . import subplots
